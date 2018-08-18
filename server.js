eabe4fad5ced4bb3d7d2f4dc4b983e68ef0562f0
gpaulbr <gpaulbr@gmail.com>
2018-08-18 17:56:50 -0300

    added basic socket communications

--- server.js
+++ server.js
@@ -1,2 +1,36 @@
 // create server connection
-const game = require('./game.js')
+const Game = require('./game.js')
+let app = require('express')()
+let http = require('http').Server(app)
+let io = require('socket.io')(http)
+let port = process.env.PORT || 3000
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/testClient.html')
+})
+
+io.on('connection', function(socket){
+    socket.on('start', function(msg){
+        let game = new Game()
+        socket.emit('gameState', game.getState())
+        socket.on('turnLeft', function() {
+            game.turnLeft()
+            socket.emit('gameState', game.getState())
+        })
+        socket.on('turnRight', function() {
+            game.turnRight()
+            socket.emit('gameState', game.getState())
+        })
+        socket.on('shoot', function() {
+            game.shoot()
+            socket.emit('gameState', game.getState())
+        })
+        setInterval(function(){
+            socket.emit('gameState', game.getState())
+        }, 20)
+    })
+})
+
+http.listen(port, function(){
+    console.log('listening on *:' + port)
+})
